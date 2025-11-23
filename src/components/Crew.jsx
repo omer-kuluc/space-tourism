a666ea5778123a9efd256f1a5521493b8980c896
omer-kuluc <omerkuluc96@gmail.com>
2025-11-23 16:51:52 +0300

    Crew, Destination, Header ve Technology componentlerine onload fonksiyonu eklendi

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react';
-import Header from './Header';
 import { DataContext } from '../App';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
@@ -7,69 +6,48 @@ import gsap from 'gsap';
 function Crew() {
   gsap.registerPlugin(useGSAP);
 
-  useGSAP(() => {
-    gsap.from(".inner-crew-area", { opacity: 0.25, duration: 1.0, ease: "power2.inOut" })
-  });
-
   const [selectedMember, setSelectedMember] = useState('commander');
-  const { data, setData } = useContext(DataContext);
-
+  const { data } = useContext(DataContext);
   const selectedMemberData = data[selectedMember] || data['commander'];
 
   useEffect(() => {
-    gsap.fromTo(".crew-image",
-      { opacity: 0 },
-      { opacity: 1, ease: "bounce" }
-    );
-
-    gsap.fromTo(".member-info",
-      { opacity: 0, y: 20 },
-      { opacity: 1, y: 0, duration: 0.5, ease: "bounce" }
-    );
-  }, [selectedMemberData]);
+    // Sayfa yüklendikten sonra animasyonları başlat
+    window.onload = function () {
+      gsap.fromTo(".crew-image", { opacity: 0 }, { opacity: 1, ease: "bounce" });
+      gsap.fromTo(".member-info", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5, ease: "bounce" });
+    };
+  }, [selectedMemberData]); // selectedMemberData değiştiğinde animasyonu başlat
 
   return (
-    <div className='crew-container'>
+    <div className="crew-container">
       <div className="inner-crew-area">
         <h1 className="crew-header">
-          <span className='crew-number'>02</span>MEET YOUR CREW
+          <span className="crew-number">02</span>MEET YOUR CREW
         </h1>
-        <img className='selectedmember-photo-mobile' src={selectedMemberData?.image} alt={selectedMemberData?.name} loading='lazy' />
+        <img
+          className="selectedmember-photo-mobile"
+          src={selectedMemberData?.image}
+          alt={selectedMemberData?.name}
+          loading="lazy"
+        />
         <div className="members-section">
           <div className="crew-section">
             <div className="crew-options">
-              <button
-                onClick={() => setSelectedMember('commander')}
-                className={`crew-option-button ${selectedMember === 'commander' ? 'selected' : ''}`}
-              >
-              </button>
-              <button
-                onClick={() => setSelectedMember('mission-specialist')}
-                className={`crew-option-button ${selectedMember === 'mission-specialist' ? 'selected' : ''}`}
-              >
-              </button>
-              <button
-                onClick={() => setSelectedMember('pilot')}
-                className={`crew-option-button ${selectedMember === 'pilot' ? 'selected' : ''}`}
-              >
-              </button>
-              <button
-                onClick={() => setSelectedMember('flight-engineer')}
-                className={`crew-option-button ${selectedMember === 'flight-engineer' ? 'selected' : ''}`}
-              >
-              </button>
+              <button onClick={() => setSelectedMember('commander')} className={`crew-option-button ${selectedMember === 'commander' ? 'selected' : ''}`}></button>
+              <button onClick={() => setSelectedMember('mission-specialist')} className={`crew-option-button ${selectedMember === 'mission-specialist' ? 'selected' : ''}`}></button>
+              <button onClick={() => setSelectedMember('pilot')} className={`crew-option-button ${selectedMember === 'pilot' ? 'selected' : ''}`}></button>
+              <button onClick={() => setSelectedMember('flight-engineer')} className={`crew-option-button ${selectedMember === 'flight-engineer' ? 'selected' : ''}`}></button>
             </div>
-
             <div className="member-info">
-              <h3 className='member-title'>{selectedMemberData?.title}</h3>
-              <h2 className='member-name'>{selectedMemberData?.name}</h2>
-              <p className='member-text'>{selectedMemberData?.text}</p>
+              <h3 className="member-title">{selectedMemberData?.title}</h3>
+              <h2 className="member-name">{selectedMemberData?.name}</h2>
+              <p className="member-text">{selectedMemberData?.text}</p>
             </div>
           </div>
-          <img className='tablet-only crew-image' src={selectedMemberData?.tabletImage} alt={selectedMemberData?.name} loading='lazy' />
+          <img className="tablet-only crew-image" src={selectedMemberData?.tabletImage} alt={selectedMemberData?.name} loading="lazy" />
         </div>
       </div>
-      <img className='desktop-only crew-image' src={selectedMemberData?.desktopImage} alt={selectedMemberData?.name} loading='lazy' />
+      <img className="desktop-only crew-image" src={selectedMemberData?.desktopImage} alt={selectedMemberData?.name} loading="lazy" />
     </div>
   );
 }
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,72 +1,85 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import { DataContext } from '../App';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+
 function Technology() {
   gsap.registerPlugin(useGSAP);
   useGSAP(() => {
-    gsap.from(".technology-section", { opacity: 0.25, duration: 0.5, ease: "power2.inOut" })
-    // gsap.from(".technology-image",
-    //   {
-    //     opacity: 0,
-    //     duration: 0.75,
-    //     ease: "power2.inOut",
-    //     clipPath: "polygon (0% 60%, 100% 60%, 100% 40%, 0% 40%)"
-    //   }
-    //   , "+=0.5")
+    gsap.from(".technology-section", { opacity: 0.25, duration: 0.5, ease: "power2.inOut" });
     gsap.utils.toArray(".desktop-only").forEach((piece, i) => {
       gsap.from(piece, {
         opacity: 0,
         y: 300,
         duration: 2,
         ease: "back",
-      })
-    })
-
-  })
-
-
+      });
+    });
+  });
 
   const [selectedVehicle, setSelectedVehicle] = useState('launch-vehicle');
-  const { data, setData } = useContext(DataContext);
-
+  const { data } = useContext(DataContext);
   const selectedVehicleData = data[selectedVehicle] || data['launch-vehicle'];
 
-  return (
-    <div className='technology-container'>
-      <div className="technology-section">
+  // Sayfa yüklendikten sonra görselleri göster
+  const handleImageLoad = () => {
+    window.onload(); // Sayfa yüklendikten sonra animasyonu başlat
+  };
 
+  return (
+    <div className="technology-container">
+      <div className="technology-section">
         <h1 className="technology-header">
-          <span className='technology-number'>02</span>SPACE LUNCH 101
+          <span className="technology-number">02</span>SPACE LUNCH 101
         </h1>
-        <img className='selected-technology-photo-mobile' src={selectedVehicleData?.image} alt={selectedVehicleData} />
-        <img className='tablet-only technology-image' src={selectedVehicleData?.tabletImage} alt={selectedVehicleData} />
+        <img
+          className="selected-technology-photo-mobile"
+          src={selectedVehicleData?.image}
+          alt={selectedVehicleData}
+          onLoad={handleImageLoad}  // Görsel yüklendikten sonra animasyonları başlat
+        />
+        <img
+          className="tablet-only technology-image"
+          src={selectedVehicleData?.tabletImage}
+          alt={selectedVehicleData}
+          onLoad={handleImageLoad}  // Görsel yüklendikten sonra animasyonları başlat
+        />
         <div className="technology-text">
           <div className="technology-options">
             <button
               className={selectedVehicle === 'launch-vehicle' ? 'selected' : ''}
-              onClick={() => setSelectedVehicle('launch-vehicle')}>1
+              onClick={() => setSelectedVehicle('launch-vehicle')}
+            >
+              1
             </button>
             <button
               className={selectedVehicle === 'spaceport' ? 'selected' : ''}
-              onClick={() => setSelectedVehicle('spaceport')}>2
+              onClick={() => setSelectedVehicle('spaceport')}
+            >
+              2
             </button>
             <button
               className={selectedVehicle === 'space-capsule' ? 'selected' : ''}
-              onClick={() => setSelectedVehicle('space-capsule')}>3
+              onClick={() => setSelectedVehicle('space-capsule')}
+            >
+              3
             </button>
           </div>
           <div className="technology-info">
             <h4>THE TERMINOLOGY ...</h4>
-            <h3 className='technology-title'>{selectedVehicleData?.title}</h3>
-            <p className='technology-text'>{selectedVehicleData?.text}</p>
+            <h3 className="technology-title">{selectedVehicleData?.title}</h3>
+            <p className="technology-text">{selectedVehicleData?.text}</p>
           </div>
         </div>
-
       </div>
-      <img className='desktop-only technology-image' src={selectedVehicleData?.desktopImage} alt={selectedVehicleData} />
+      <img
+        className="desktop-only technology-image"
+        src={selectedVehicleData?.desktopImage}
+        alt={selectedVehicleData}
+        onLoad={handleImageLoad}  // Görsel yüklendikten sonra animasyonları başlat
+      />
     </div>
-  )
+  );
 }
 
 export default Technology;
