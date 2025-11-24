339a242491d5e68cd00ecc9587a6941b4fc5d2ea
omer-kuluc <omerkuluc96@gmail.com>
2025-11-24 14:03:00 +0300

    loading sayfasında gsap animasyonları eklendi

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,32 @@
 // src/components/Loading.jsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 
 function Loading() {
+  const loadingRef = useRef(null);  // Ref tanımlıyoruz
+
+  useEffect(() => {
+    // Sayfa yüklendikten sonra animasyon başlasın
+    gsap.fromTo(
+      loadingRef.current, // Ref'e bağlanan DOM öğesi
+      {
+        opacity: 1,
+        duration: 4, // Animasyon süresi 1 saniye
+
+      }, // Başlangıç opaklık 1
+      {
+        opacity: 0, // Sonra opaklık 0 olacak
+        delay: 3, // 2 saniye bekleyip animasyon başlasın
+        onComplete: () => {
+          // Animasyon tamamlandığında component'i kaldır
+          loadingRef.current.style.display = 'none';
+        }
+      }
+    );
+  }, []);
+
   return (
-    <div className="loading-container">
+    <div className="loading-container" ref={loadingRef}>
       <div className="spinner">Yükleniyor...</div>
     </div>
   );
