e9d96e9f49478baf2eb45b5593fede8c50244b1e
omer-kuluc <omerkuluc96@gmail.com>
2025-11-24 13:32:19 +0300

    loading sayfası eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
-import { createContext, useEffect, useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Destination from './components/Destination'
-import Crew from './components/Crew'
-import Technology from './components/Technology'
-import Header from './components/Header'
+import { createContext, useEffect, useState } from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Destination from './components/Destination';
+import Crew from './components/Crew';
+import Technology from './components/Technology';
+import Header from './components/Header';
+import Loading from './components/Loading';  // Loading bileşenini dahil ediyoruz
 
 export const DataContext = createContext(null);
 
-
 function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Loading durumunu başlatıyoruz
 
   useEffect(() => {
     async function getData() {
       const data = await fetch('/data/data.json').then(x => x.json());
       setData(data);
+      setIsLoading(false); // Veriler yüklendiğinde loading ekranını kapatıyoruz
     }
     getData();
-
   }, []);
-
 
   return (
     <DataContext.Provider value={{ data, setData }}>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/destination' element={<Destination />} />
-        <Route path='/crew' element={<Crew />} />
-        <Route path='/technology' element={<Technology />} />
-      </Routes>
-
+      {isLoading ? (
+        <Loading /> // Yükleme sırasında Loading bileşeni gösteriliyor
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      )}
     </DataContext.Provider>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+// src/components/Loading.jsx
+import React from 'react';
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="spinner">Yükleniyor...</div>
+    </div>
+  );
+}
+
+export default Loading;
