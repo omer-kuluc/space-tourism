3f572c02811bce40b59d93cdff02defc4518bfd6
omer-kuluc <omerkuluc96@gmail.com>
2025-11-24 14:30:20 +0300

    loading sayfasında güncelleme yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import Destination from './components/Destination';
 import Crew from './components/Crew';
 import Technology from './components/Technology';
-import Header from './components/Header';
+import Header from './components/Header';  // Header bileşenini dahil ediyoruz
 import Loading from './components/Loading';  // Loading bileşenini dahil ediyoruz
 
 export const DataContext = createContext(null);
@@ -18,23 +18,27 @@ function App() {
     async function getData() {
       const data = await fetch('/data/data.json').then(x => x.json());
       setData(data);
-      setIsLoading(false); // Veriler yüklendiğinde loading ekranını kapatıyoruz
+
+      // 4.5 saniye sonra loading ekranını kapatıyoruz
+      setTimeout(() => setIsLoading(false), 2500); // 2.5 saniye bekle
     }
     getData();
   }, []);
 
   return (
     <DataContext.Provider value={{ data, setData }}>
-      <Header />
       {isLoading ? (
         <Loading /> // Yükleme sırasında Loading bileşeni gösteriliyor
       ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
-        </Routes>
+        <>
+          <Header />  {/* Veriler yüklendikten sonra Header'ı gösteriyoruz */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/destination" element={<Destination />} />
+            <Route path="/crew" element={<Crew />} />
+            <Route path="/technology" element={<Technology />} />
+          </Routes>
+        </>
       )}
     </DataContext.Provider>
   );
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,22 @@
 // src/components/Loading.jsx
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-import { useGSAP } from '@gsap/react';
 
 function Loading() {
   const loadingRef = useRef(null);  // Ref tanımlıyoruz
 
-  gsap.registerPlugin(useGSAP);
-
-
-  useGSAP(() => {
-    gsap.from(".loading-container", { opacity: 0, duration: 4.5, ease: "power2.inOut" })
-  })
+  useEffect(() => {
+    // Sayfa yüklendikten sonra animasyon başlasın
+    gsap.fromTo(
+      loadingRef.current, // Ref'e bağlanan DOM öğesi
+      { opacity: 1 }, // Başlangıç opaklık 1
+      {
+        opacity: 0, // Sonra opaklık 0 olacak
+        duration: 2.5, // Animasyon süresi 4.5 saniye
+        ease: "power2.inOut", // Animasyon easing
+      }
+    );
+  }, []);
 
   return (
     <div className="loading-container" ref={loadingRef}>
