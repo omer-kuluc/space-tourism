5b20c392c50110129109f985930e076049d8f383
omer-kuluc <omerkuluc96@gmail.com>
2025-11-24 14:24:17 +0300

    loading sayfasında animasyon güncellemesi yapıldı

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,29 +1,17 @@
 // src/components/Loading.jsx
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 function Loading() {
   const loadingRef = useRef(null);  // Ref tanımlıyoruz
 
-  useEffect(() => {
-    // Sayfa yüklendikten sonra animasyon başlasın
-    gsap.fromTo(
-      loadingRef.current, // Ref'e bağlanan DOM öğesi
-      {
-        opacity: 1,
-        duration: 4, // Animasyon süresi 1 saniye
+  gsap.registerPlugin(useGSAP);
 
-      }, // Başlangıç opaklık 1
-      {
-        opacity: 0, // Sonra opaklık 0 olacak
-        delay: 3, // 2 saniye bekleyip animasyon başlasın
-        onComplete: () => {
-          // Animasyon tamamlandığında component'i kaldır
-          loadingRef.current.style.display = 'none';
-        }
-      }
-    );
-  }, []);
+
+  useGSAP(() => {
+    gsap.from(".loading-container", { opacity: 0, duration: 4.5, ease: "power2.inOut" })
+  })
 
   return (
     <div className="loading-container" ref={loadingRef}>
